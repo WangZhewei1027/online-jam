633a97ab657b4d49dce62522e65ea549363b5def
王哲玮 <m13636472690@163.com>
2024-09-29 15:21:29 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Let's Jam",
+  description: "A collaborative music-making app",
 };
 
 export default function RootLayout({
